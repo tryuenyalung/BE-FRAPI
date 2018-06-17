bef549b148f29a09c109d187702ec329da9f4ef1
tryuenyalung <yuen.yalung@tooltwist.com>
2018-06-17 22:53:38 +0800

    remove callback hell from authcontroller

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,29 +1,52 @@
 import Users from './../schemas/Users'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import keys from './../keys'
 
 //authenticate user
 export const authUser =(req, res)=> {
+    const username = {username : req.body.username}
+
+    const cbGetUser =(err, userData)=> {
+        if(err){
+         res.status(404).send({errors: err})
+        }else if(userData === null){//check if no user match the credentials
+         res.status(401).send({errors: "no record found in database"})
+        }else{
+            
+             if(!userData.status){//check if status is active
+                 res.status(401).send({errors: "your account is registered, but we still need approval of the admin, please contact your system administrator"})
+             }else{
+                
+                 const cbSendToken =(err, token)=> {//send token
+                    const dataForLocalStorage= {
+                        token: token,
+                        user: userData
+                    }
+                    res.send(dataForLocalStorage)
+                }//cbSendToken
+
+                const cbCheckPassword =(err, isPasswordValid) => {// check if password valid and send token
+                    !isPasswordValid ? res.status(401).send({errors : "invalid password"}) 
+                    :jwt.sign( {id : userData._id} , keys.SECRET, cbSendToken)
+                }
+                
+                //check if password matches
+                bcrypt.compare(req.body.password, userData.password, cbCheckPassword)
+             }
+        }//else
+        
+    }//cbGetUser
 
     //findOne by username
-    Users.findOne( {username : req.body.username} , (err, userData) => {
+    Users.findOne(username, cbGetUser)//findOne
 
-       if(err){
-        res.status(404).send({errors: err})
-       }else if(userData === null){//check if no user match the credentials
-        res.status(401).send({errors: "no record found in database"})
-       }else{
 
-            if(!userData.status){//check if status is active
-                res.status(401).send({errors: "your account is registered, but we still need approval of the admin, please contact your system administrator"})
-            }else{
-                //check if password matches
-                bcrypt.compare(req.body.password, userData.password).then( (isPasswordValid) => {
-                    isPasswordValid ? res.status(200).send("valid user") : res.status(401).send({errors : "invalid password"})
-                })
-            }
 
-       }//else
+ 
+ 
 
-    })//findOne
+  
 
 }//@end
+ 
--- src/keys.js
+++ src/keys.js
@@ -2,6 +2,8 @@ export default {
 
     "LOCAL_PORT": 5000,
 
+    "SECRET": "10bjklnasldknoai0981y2laskdn",
+
     "USERS_DB": "mongodb://root:root@ds149134.mlab.com:49134/mydb",
 
     "DEFAULT_AVATAR" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF1zJt89f-BE-BEriCrVlTfXhOKdTnVzo-CdfbL4S9aC4gzA03",
